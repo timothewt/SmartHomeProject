b0c775ed832ca2ad8989bd345f1b3712c58fb906
timothewt <timothe.wt@gmail.com>
2022-12-18 19:24:15 +0100

    changed room update method

--- src/model/Room.java
+++ src/model/Room.java
@@ -6,79 +6,122 @@
  */
 package model;
 
-public class Room 
-{
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class Room {
 
 	/*
-	 * Attributs
+	 * Attributes
 	 */
-	private int heaterTemperature;
-	private int temperature;
-	
-	/*
-	 * Rate are set to stay between 0 and 1
-	 */
-	private float humidityRate;
+	private String name;
+	private float heaterTemperature;
+	private boolean isHeaterTurnedOn;
+	private float temperature;
+	private float humidityRate; // Rates are set to stay between 0 and 1
 	private float isolationRate;
+	private boolean isWindowOpen;
 
 	/*
-	 * @brief Default constructor
+	 * @brief Constructors
 	 */
-	public Room()
-	{
-		this.heaterTemperature = 0;
-		this.temperature = 18;
-		this.humidityRate = 0.1f;
-		this.isolationRate = 0.5f;
+	public Room() {
+		this.heaterTemperature = 0f;
+		this.isHeaterTurnedOn = false;
+		this.temperature = 18f;
+		this.humidityRate = .1f;
+		this.isolationRate = .5f;
+		this.isWindowOpen = false;
 	}
-	
-	/*
-	 * @brief Constructor
-	 */
-	public Room(int heaterTemperature, int temperature, float humidityRate, float isolationRate) 
-	{
+
+	public Room(String name, float heaterTemperature, boolean isHeaterTurnedOn, float temperature, float humidityRate, float isolationRate, boolean isWindowOpen) {
+		this.name = name;
 		this.heaterTemperature = heaterTemperature;
+		this.isHeaterTurnedOn = isHeaterTurnedOn;
 		this.temperature = temperature;
 		this.humidityRate = humidityRate;
 		this.isolationRate = isolationRate;
+		this.isWindowOpen = isWindowOpen;
 	}
 
-
 	/*
-	 * 
-	 * @brief Update the humidityRate according to the outside humidityRate
-	 * @param float humidityRate : Weather's humidityRate
+	 * @brief Updates the room stats according to outsideHumidityRate and the temperature
+	 * @param float outsideHumidityRate : Weather's outsideHumidityRate
 	 * @return void
 	 */
-	public void updateStats(float humidityRate)
-	{
-		if (this.humidityRate > humidityRate)
-		{
-			this.humidityRate = this.humidityRate + (1 - this.isolationRate) * humidityRate;
+	public void updateStats(float outsideHumidityRate, float outsideTemperature) {
+
+		if (this.isWindowOpen) {
+
+			this.humidityRate = (this.humidityRate + outsideHumidityRate) / 2;
+			this.temperature = (this.temperature + outsideTemperature) / 2;
+
+		} else {
+
+			this.temperature = isHeaterTurnedOn ? (this.temperature + this.heaterTemperature) / 2 : this.temperature;
+			// Loss of humidity and temperature due to non-optimal isolation
+			int humidityChangeSign = this.humidityRate > outsideHumidityRate ? -1 : 1;
+			int temperatureChangeSign = this.temperature > outsideTemperature ? -1 : 1;
+			this.humidityRate = max(min(this.humidityRate + humidityChangeSign * (1 - this.isolationRate) * outsideHumidityRate, 1), 0);
+			this.temperature = this.temperature + temperatureChangeSign * (1 - this.isolationRate) * outsideTemperature;
+
 		}
-		else if (this.humidityRate < humidityRate)
-		{
-			this.humidityRate = this.humidityRate - (1 - this.isolationRate) * humidityRate;
-		}
-		
-		/*
-		 * Fix the value in the definition range
-		 */
-		if (this.humidityRate <= 0)
-		{
-			this.humidityRate = 0;
-		}
-		else if (this.humidityRate >0)
-		{
-			this.humidityRate = 1;
-		}
+
 	}
-	
-	/*
-	 * @brief Modify the value of heaterTemperature
-	 */
-	public void setHeaterTemperature(int heaterTemperature) 
-	{
+
+	public float getHeaterTemperature() {
+		return heaterTemperature;
+	}
+
+	public void setHeaterTemperature(float heaterTemperature) {
 		this.heaterTemperature = heaterTemperature;
+	}
+
+	public boolean isHeaterTurnedOn() {
+		return isHeaterTurnedOn;
+	}
+
+	public void setHeaterTurnedOn(boolean heaterTurnedOn) {
+		isHeaterTurnedOn = heaterTurnedOn;
+	}
+
+	public float getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(float temperature) {
+		this.temperature = temperature;
+	}
+
+	public float getHumidityRate() {
+		return humidityRate;
+	}
+
+	public void setHumidityRate(float humidityRate) {
+		this.humidityRate = max(min(humidityRate, 1), 0);;
+	}
+
+	public float getIsolationRate() {
+		return isolationRate;
+	}
+
+	public void setIsolationRate(float isolationRate) {
+		this.isolationRate = max(min(isolationRate, 1), 0);
+	}
+
+	public boolean isWindowOpen() {
+		return isWindowOpen;
+	}
+
+	public void setWindowOpen(boolean windowOpen) {
+		isWindowOpen = windowOpen;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
