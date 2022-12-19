acbd2239ea19ee999e559d63bd647aee7070c261
timothewt <timothe.wt@gmail.com>
2022-12-19 14:58:24 +0100

    started implementing update method

--- src/model/House.java
+++ src/model/House.java
@@ -18,6 +18,8 @@ public class House {
 	private ArrayList<PowerGenerator> powerSupply;
 	private ArrayList<Perk> perks;
 	private Couple couple;
+	private float optimalTemperature;
+	private float optimalHumidityRate;
 	
 	/**
 	 * Constructors
@@ -28,13 +30,15 @@ public class House {
 		this.energy = 0;
 		this.isOnPowerOutage = false;
 		this.perks = new ArrayList<Perk>();
+		this.optimalTemperature = 21f;
+		this.optimalHumidityRate = .45f;
 
 		initRooms();
 		initPowerSupply();
 		initCouple();
 	}
 
-	public House(int temperature, int humidityRate, int energy, boolean isOnPowerOutage, ArrayList<Room> rooms, ArrayList<PowerGenerator> powerSupply, ArrayList<Perk> perks, Couple couple) {
+	public House(int temperature, int humidityRate, int energy, boolean isOnPowerOutage, ArrayList<Room> rooms, ArrayList<PowerGenerator> powerSupply, ArrayList<Perk> perks, Couple couple, float optimalTemperature, float optimalHumidityRate) {
 		this.temperature = temperature;
 		this.humidityRate = humidityRate;
 		this.energy = energy;
@@ -43,28 +47,8 @@ public class House {
 		this.powerSupply = powerSupply;
 		this.perks = perks;
 		this.couple = couple;
-	}
-
-	/**
-	 * Used to initialize the power supplies of the house
-	 */
-	private void initPowerSupply() {
-		this.powerSupply = new ArrayList<PowerGenerator>();
-		PowerGenerator linky = new PowerGenerator("Linky", 0, 2, 1);
-		this.powerSupply.add(linky);
-	}
-	private void initCouple() {
-		Person jean = new Person("Jean", 0, 10, new ArrayList<Task>());
-		Person marie = new Person("Marie", 1, 10, new ArrayList<Task>());
-		
-		this.couple.addPerson(jean);
-		this.couple.addPerson(marie);
-	}
-	private void initRooms() {
-		this.rooms = new ArrayList<Room>();
-		Room kitchen = new Room();
-
-		this.rooms.add(kitchen);
+		this.optimalTemperature = optimalTemperature;
+		this.optimalHumidityRate = optimalHumidityRate;
 	}
 	
 	/**
@@ -72,11 +56,14 @@ public class House {
 	 * @return true if the house is still viable, false otherwise.
 	 */
 	public boolean isViable() {
-		return temperature < 40 && temperature > 10 && humidityRate < .5;
+		return temperature < optimalTemperature + 10 &&
+				temperature > optimalTemperature - 10 &&
+				humidityRate < optimalHumidityRate + .15 &&
+				humidityRate < optimalHumidityRate - .15;
 	}
 
 	/**
-	 * Updates all the house elements according to the current weather
+	 * Updates all the house elements according to the current weather and perks
 	 * @param weather: weather of the environment
 	 */
 	public void update(Weather weather) {
@@ -94,14 +81,76 @@ public class House {
 			this.isOnPowerOutage = true;
 		}
 
-		// fonctions avec perks, fenetres auto par exemple
+		for (Perk perk: perks) {
+			switch (perk.getId()) {
+				case 0: // automatic windows
+					this.energy -= 10;
+					this.setAllWindowsOpen(temperature < this.optimalTemperature && temperature < weather.getTemperature()); // if the temperature is better outside
+				case 1: // automatic heaters
+					this.energy -= 10;
+					if (temperature < this.optimalTemperature && !this.rooms.get(0).isWindowOpen()) {
+						this.setAllHeatersTemperature(this.optimalTemperature);
+					} else {
+						this.turnOffAllHeaters();
+					}
+					break;
+			}
+		}
 
+		for (Room room: rooms) {
+			if (room.isHeaterTurnedOn()) {
+				this.energy -= 1;
+			}
+		}
+
+	}
+
+	private void setAllWindowsOpen(boolean status) {
+		for (Room room: rooms) {
+			room.setWindowOpen(status);
+		}
+	}
+
+	private void setAllHeatersTemperature(float temperature) {
+		for (Room room: rooms) {
+			room.setHeaterTemperature(temperature);
+			room.setHeaterTurnedOn(true);
+		}
+	}
+
+	private void turnOffAllHeaters() {
+		for (Room room: rooms) {
+			room.setHeaterTurnedOn(false);
+		}
 	}
 
 	public void onNewDay() {
 		for (PowerGenerator powerGenerator: this.powerSupply) {
 			this.energy += powerGenerator.getDailyProduction();
+			this.couple.setMoney(this.couple.getMoney() - powerGenerator.getDailyCost());
 		}
+	}
+
+	/**
+	 * Used to initialize the power supplies of the house
+	 */
+	private void initPowerSupply() {
+		this.powerSupply = new ArrayList<PowerGenerator>();
+		PowerGenerator linky = new PowerGenerator("Linky", 0, 2, 100);
+		this.powerSupply.add(linky);
+	}
+	private void initCouple() {
+		Person jean = new Person("Jean", 0, 10, new ArrayList<Task>());
+		Person marie = new Person("Marie", 1, 10, new ArrayList<Task>());
+
+		this.couple.addPerson(jean);
+		this.couple.addPerson(marie);
+	}
+	private void initRooms() {
+		this.rooms = new ArrayList<Room>();
+		Room kitchen = new Room();
+
+		this.rooms.add(kitchen);
 	}
 
 	public String toString() {
