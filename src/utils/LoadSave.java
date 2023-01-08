7c2ee3898164d0b3cd59970496016026b58930a1
tmayer54 <thibault.mayer54@gmail.com>
2023-01-08 19:55:51 +0100

    Exe final + retrait d'un Sysout

--- src/utils/LoadSave.java
+++ src/utils/LoadSave.java
@@ -12,13 +12,12 @@ import model.Weather;
 
 public class LoadSave {
 
-
 	/**
 	 * @brief Create a new file named game.txt if this file does not exist yet
 	 */
 	public static void CreateFile() {
 		File txtFile = new File("game.txt");
-		
+
 		try {
 			txtFile.createNewFile();
 		} catch (IOException e) {
@@ -68,25 +67,24 @@ public class LoadSave {
 	}
 
 	public static Game LoadGame() {
-		ArrayList<Integer> save = ReadFromFile(new File("game"+".txt"));
-		float weatherTemperature = (float)save.get(3)/100;
-		float weatherHumidity = (float)save.get(4)/100;
+		ArrayList<Integer> save = ReadFromFile(new File("game" + ".txt"));
+		float weatherTemperature = (float) save.get(3) / 100;
+		float weatherHumidity = (float) save.get(4) / 100;
 		Weather weather = new Weather(weatherTemperature, weatherHumidity);
-		switch(save.get(5)) {
-			case 0 -> weather.setRainy(true);
-			case 1 -> weather.setSnowy(true);
-			case 2 -> weather.setSunny(true);
-			case 3 -> weather.setLightning(true);
+		switch (save.get(5)) {
+		case 0 -> weather.setRainy(true);
+		case 1 -> weather.setSnowy(true);
+		case 2 -> weather.setSunny(true);
+		case 3 -> weather.setLightning(true);
 		}
-		
-		Game game = new Game(save.get(0), weather);	
+
+		Game game = new Game(save.get(0), weather);
 		game.getHouse().getFamily().setMoney(save.get(1));
 		game.getHouse().setEnergy(save.get(2));
-		game.getHouse().setTemperature((float)save.get(6)/100);
-		game.getHouse().setHumidityRate((float)save.get(7)/100);
+		game.getHouse().setTemperature((float) save.get(6) / 100);
+		game.getHouse().setHumidityRate((float) save.get(7) / 100);
 		for (int i = 0; i < save.get(8); i++) {
-			game.addPerkFromID(save.get(i+9));
-			System.out.println(save.get(i+9));
+			game.addPerkFromID(save.get(i + 9));
 		}
 		return game;
 	}
